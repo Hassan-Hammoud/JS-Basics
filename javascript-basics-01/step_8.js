673ab1cf7cb8a63d0403673ecf5a33b186cdeb65
Hassan Hammoud <hassanhammoud2000@gmail.com>
2022-12-08 10:42:24 +0000

    basics-01-style_8/finish-basics

---
+++ javascript-basics-01/step_8.js
@@ -0,0 +1,6 @@
+document.getElementById("validate").onclick = function () {
+  yourage = document.getElementById("age").value;
+  if (yourage > 18) alert("you are over 18");
+  else if ((yourage = 18)) alert("you are 18 years old");
+  else if (yourage < 18) alert("you are under 18");
+};
