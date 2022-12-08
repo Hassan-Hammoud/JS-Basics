552e29ebeadf3fc10b2f502914acd87120c49578
Hassan Hammoud <hassanhammoud2000@gmail.com>
2022-12-08 18:21:28 +0000

    basics-02-step_2

--- javascript-basics-01/step_4.js
+++ javascript-basics-01/step_4.js
@@ -1,9 +1,9 @@
 document.getElementById("validate").onclick = function () {
-  name = document.getElementById("name").value;
+  Name = document.getElementById("name").value;
   surname = document.getElementById("surname").value;
   city = document.getElementById("city").value;
 
   alert(
-    "Name : " + name + "\n" + "Surname : " + surname + "\n" + "City : " + city
+    "Name : " + Name + "\n" + "Surname : " + surname + "\n" + "City : " + city
   );
 };
