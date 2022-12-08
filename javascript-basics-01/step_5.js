464c1d85a5f92d02767c950a7e0b11c9a27b6ad9
Hassan Hammoud <hassanhammoud2000@gmail.com>
2022-12-08 09:21:46 +0000

    basics-01-style_5

---
+++ javascript-basics-01/step_5.js
@@ -0,0 +1,5 @@
+document.getElementById("validate").onclick = function () {
+  firstnumber = document.getElementById("first_number").value;
+  secondnumber = document.getElementById("second_number").value;
+  alert("the Result : " + firstnumber * secondnumber);
+};
